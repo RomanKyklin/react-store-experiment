5dbf6ee63a47f1238cf2423e23ce9b76a6973770
roman.kyklin <sk.trac@yandex.ru>
2019-09-05 19:01:53 +0500

    mini fix

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,14 +1,9 @@
 import {initialState} from "./reducer";
-import {SET_ERROR, SET_IS_AUTH, SET_LOADING} from "../constants/action-types";
+import {SET_ERROR, SET_LOADING} from "../constants/action-types";
 
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SET_IS_AUTH:
-            return {
-                ...state,
-                isAuth: action.isAuth
-            };
         case SET_ERROR:
             return {
                 ...state,
