99423d1d7fece5ffcb07b531fca226e1af179be4
roman.kyklin <sk.trac@yandex.ru>
2019-09-09 15:59:18 +0500

    #9  * work on server refactoring

--- server/app/controllers/product.controller.js
+++ server/app/controllers/product.controller.js
@@ -1,65 +1,42 @@
 const _ = require('lodash');
-const Product = require('../models/product.model.js');
-const Category = require('../models/category.model');
 const productService = require('../services/product.service');
 
-exports.find = (req, res) => {
-    if (!req.query.id) {
+exports.find = async (req, res) => {
+    const id = _.get(req.query, 'id', null);
+
+    if (!id) {
         return res.status(400).send({
             message: "Product id can not be empty"
         });
     }
 
-    Product.findById(req.query.id)
-        .populate('category')
-        .then(product => {
-            if (!product) {
-                return res.status(400).send({
-                    message: "Product not found!"
-                });
-            }
-            res.send(product);
-        })
-        .catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Product not found with id " + req.params.id
-                });
-            }
-            return res.status(500).send({
-                message: "Error finding product with id " + req.params.id
-            });
-        })
+    try {
+        const product = await productService.getProductById(id);
+        return res.send(product);
+    } catch (e) {
+        return res.status(500).send({
+            message: e.message || "Error finding product with id " + id
+        });
+    }
 };
 
 exports.findAll = async (req, res) => {
     const page = parseInt(_.get(req.query, 'page', 1));
     const perPage = parseInt(_.get(req.query, 'perPage', 10));
     const filterCategoryId = _.get(req.query, 'filterCategoryId', null);
-    console.log(filterCategoryId);
+
     try {
-        if (filterCategoryId) {
-            const products = await productService.getProducts({category: filterCategoryId}, perPage, page);
-            const productCount = await productService.getProductsCount({category: filterCategoryId});
+        const products = filterCategoryId ? await productService.getProducts({category: filterCategoryId}, perPage, page)
+            : await productService.getProducts({}, perPage, page);
+        const productCount = filterCategoryId ? await productService.getProductsCount({category: filterCategoryId})
+            : await productService.getProductsCount();
 
-            return res.send({
-                products,
-                page,
-                pagesCount: Math.ceil(productCount / perPage),
-                total: productCount
-            })
-        } else {
-            const products = await productService.getProducts({}, perPage, page);
-            const productCount = await productService.getProductsCount();
-
-            return res.send({
-                products,
-                page,
-                pagesCount: Math.ceil(productCount / perPage),
-                total: productCount
-            })
-        }
-
+        return res.send({
+            products,
+            page,
+            pagesCount: Math.ceil(productCount / perPage),
+            total: productCount
+        })
     } catch (e) {
         res.status(500).send({
             message: e.message || "Some error occurred while retrieving products."
@@ -67,92 +44,75 @@ exports.findAll = async (req, res) => {
     }
 };
 
-exports.create = (req, res) => {
-    if (!req.body.title || !req.body.sellingPrice || !req.body.purchasePrice || !req.body.category) {
+exports.create = async (req, res) => {
+    const title = _.get(req.body, 'title', null);
+    const sellingPrice = _.get(req.body, 'sellingPrice', null);
+    const purchasePrice = _.get(req.body, 'purchasePrice', null);
+    const category = _.get(req.body, 'category', null);
+
+    if (!title || !sellingPrice || !purchasePrice || !category) {
         return res.status(400).send({
             message: "Product content can not be empty"
         });
     }
 
-    const product = new Product({
-        title: req.body.title,
-        selling_price: req.body.sellingPrice,
-        purchase_price: req.body.purchasePrice,
-        category: req.body.category,
-    });
-
-    product.save()
-        .then(data => {
-            res.send(data);
-        }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the product."
-        });
-    });
+    try {
+        const product = await productService.createProduct(title, sellingPrice, purchasePrice, category);
+        return res.send(product);
+    } catch (e) {
+        return res.status(500).send({
+            message: e.message || "Some error occurred while creating the product."
+        })
+    }
 };
 
-exports.change = (req, res) => {
-    if (!req.body.id || !req.body.title || !req.body.sellingPrice || !req.body.purchasePrice || !req.body.category) {
+exports.change = async (req, res) => {
+    const id = _.get(req.body, 'id', null);
+    const title = _.get(req.body, 'title', null);
+    const sellingPrice = _.get(req.body, 'sellingPrice', null);
+    const purchasePrice = _.get(req.body, 'purchasePrice', null);
+    const category = _.get(req.body, 'category', null);
+
+    if (!id || !title || !sellingPrice || !purchasePrice || !category) {
         return res.status(400).send({
             message: "Product content can not be empty"
         });
     }
 
-    Product.findByIdAndUpdate(req.body.id, {
-        title: req.body.title,
-        selling_price: req.body.sellingPrice,
-        purchase_price: req.body.purchasePrice,
-        category: req.body.category,
-    }, {new: true})
-        .then(product => {
-            if (!product) {
-                return res.status(404).send({
-                    message: "Product not found with id " + req.params.id
-                });
-            }
-            res.send(product);
-        })
-        .catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Product not found with id " + req.params.id
-                });
-            }
-            return res.status(500).send({
-                message: "Error updating product with id " + req.params.id
-            });
-        })
-
+    try {
+        const product = await productService.changeProduct(id, title, sellingPrice, purchasePrice, category);
+        return res.send(product);
+    } catch (e) {
+        return res.status(500).send({
+            message: e.message || "Error updating product with id " + req.params.id
+        });
+    }
 };
 
-exports.delete = (req, res) => {
-    if (!req.body.id) {
+exports.delete = async (req, res) => {
+    const id = _.get(req.body, 'id', null);
+
+    if (!id) {
         return res.status(400).send({
             message: "Product id can not be empty"
         });
     }
 
-    Product.findByIdAndDelete(req.body.id)
-        .then(product => {
-                if (!product) {
-                    return res.status(404).send({
-                        message: "Product not found with id " + req.params.id
-                    });
-                }
+    try {
+        const product = await productService.deleteProduct(id);
 
-                res.status(200).send({
-                    message: "product removed successfully with id " + req.body.id
-                })
-            }
-        )
-        .catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Product not found with id " + req.params.id
-                });
-            }
-            return res.status(500).send({
-                message: "Error deleting product with id " + req.params.id
+        if (!product) {
+            return res.status(404).send({
+                message: "Product not found with id " + id
             });
+        }
+
+        res.status(200).send({
+            message: "product removed successfully with id " + id
         })
+    } catch (e) {
+        return res.status(500).send({
+            message: e.message || "Error deleting product with id " + id
+        });
+    }
 };
--- server/app/services/product.service.js
+++ server/app/services/product.service.js
@@ -18,10 +18,79 @@ exports.getProducts = (query = {}, perPage = 10, page = 1) => {
     }
 };
 
+/**
+ * @param id
+ * @returns {Query}
+ */
+exports.getProductById = id => {
+    try {
+        return Product.findById(id).populate('category');
+    } catch (e) {
+        throw Error("Error finding product with id " + id);
+    }
+};
+
+/**
+ * Get count of products in the database
+ * @param {Object} query
+ * @returns {Query}
+ */
 exports.getProductsCount = (query = {}) => {
     try {
         return Product.countDocuments(query);
     } catch (e) {
-        throw Error("Some error occurred while retrieving products count.")
+        throw Error("Some error occurred while retrieving products count.");
+    }
+};
+
+/**
+ * @param title
+ * @param sellingPrice
+ * @param purchasePrice
+ * @param category
+ * @returns {*}
+ */
+exports.createProduct = (title = null, sellingPrice = null, purchasePrice = null, category = null) => {
+    try {
+        const product = new Product({
+            title,
+            selling_price: sellingPrice,
+            purchase_price: purchasePrice,
+            category,
+        });
+
+        return product.save();
+    } catch (e) {
+        throw Error("Some error occurred while creating the product.")
+    }
+
+};
+
+/**
+ * @param id
+ * @param title
+ * @param sellingPrice
+ * @param purchasePrice
+ * @param category
+ * @returns {Query}
+ */
+exports.changeProduct = (id, title = null, sellingPrice = null, purchasePrice = null, category = null) => {
+    try {
+        return Product.findByIdAndUpdate(id, {
+            title,
+            selling_price: sellingPrice,
+            purchase_price: purchasePrice,
+            category,
+        }, {new: true});
+    } catch (e) {
+        throw Error("Error updating product with id " + id)
+    }
+};
+
+exports.deleteProduct = id => {
+    try {
+        return Product.findByIdAndDelete(id);
+    } catch (e) {
+        throw Error("Error deleting product with id " + id);
     }
 };
